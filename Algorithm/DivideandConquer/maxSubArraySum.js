5b986bc785e4cb1f2a84a50bbbd99dc5075e52a3
sharan sharma <sharansharma19@gmail.com>
2021-12-07 16:51:07 +0530

    BruteForce Solution added and tested

--- Algorithm/DivideandConquer/maxSubArraySum.js
+++ Algorithm/DivideandConquer/maxSubArraySum.js
@@ -1,24 +1,21 @@
-const bruteForce = (input) => {
-  let MaxSum = 0;
-  let start = 0,
-    end = 0;
-  for (let i = 0; i < input.length; i++) {
-    const purchaseAt = input[i];
-    for (let j = i + 1; j < input.length; j++) {
-      const profit = input[j] - purchaseAt;
-      if (profit > MaxSum) {
-        MaxSum = profit;
-        start = i;
-        end = j;
+const testcases = require("./testcase.js");
+
+const maxSubArray = function (array) {
+  if (array.length < 1) return;
+  if (array.length === 1) return array[0];
+  let max = array[0];
+  for (let index = 0; index < array.length; index++) {
+    let sum = 0;
+    for (let j = index; j < array.length; j++) {
+      sum = sum + array[j];
+      if (sum > max) {
+        max = sum;
       }
     }
   }
-  return { MaxSum, start, end };
+  return max;
 };
 
-// const input = [0, 13, -5, 25, -10, -11, 2];
-const input = [
-  13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
-];
-
-console.log(bruteForce(input));
+for (let testcase of testcases) {
+  console.log(maxSubArray(testcase));
+}
