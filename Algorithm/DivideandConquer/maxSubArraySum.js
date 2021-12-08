ce1e7657f536bc7964b311c2cd01f8f05a63d90d
sharan sharma <sharansharma19@gmail.com>
2021-12-08 22:41:23 +0530

    n log n solution added

--- Algorithm/DivideandConquer/maxSubArraySum.js
+++ Algorithm/DivideandConquer/maxSubArraySum.js
@@ -1,6 +1,6 @@
 const testcases = require("./testcase.js");
-
-const maxSubArray = function (array) {
+//n^2
+const maxSubArrayWorst = function (array) {
   if (array.length < 1) return;
   if (array.length === 1) return array[0];
   let max = array[0];
@@ -16,6 +16,49 @@ const maxSubArray = function (array) {
   return max;
 };
 
+//O(n log n)
+const findMidCrossMax = (array, start, end, mid) => {
+  let leftMax = Number.NEGATIVE_INFINITY;
+  let sum = 0;
+  for (let index = mid; index >= start; index--) {
+    sum += array[index];
+    if (sum > leftMax) leftMax = sum;
+  }
+
+  let rightMax = Number.NEGATIVE_INFINITY;
+  sum = 0;
+  for (let index = mid + 1; index <= end; index++) {
+    sum += array[index];
+    if (sum > rightMax) rightMax = sum;
+  }
+
+  return leftMax + rightMax;
+};
+
+const findMaxSum = (array, start, end) => {
+  //base case
+  if (start === end) return array[start];
+
+  const mid = Math.floor((end + start) / 2);
+  // calculate left maximun
+  const leftMax = findMaxSum(array, start, mid);
+  // calculate right max
+  const rightMax = findMaxSum(array, mid + 1, end);
+  //  calculate both max
+  const crossMax = findMidCrossMax(array, start, end, mid);
+
+  if (leftMax >= rightMax && leftMax >= crossMax) {
+    return leftMax;
+  } else if (rightMax >= leftMax && rightMax >= crossMax) {
+    return rightMax;
+  } else return crossMax;
+};
+
+const maxSubArray = function (array) {
+  return findMaxSum(array, 0, array.length - 1);
+};
+
+// maxSubArray(testcases[0]);
 for (let testcase of testcases) {
   console.log(maxSubArray(testcase));
 }
